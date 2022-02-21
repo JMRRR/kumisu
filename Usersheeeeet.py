5280a6a778cc239b069612d586b8d8ebc571c0d9
JMRRR <jan-manoel.rumitz@mellowmessage.de>
2022-02-21 21:11:51 +0100

    first commit

---
+++ Usersheeeeet.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+      
+import os
+from base64 import b64encode
+#geniert Passworter rundommäßig
+
+V="Loumi"
+N="Rumitz"
+key = os.urandom(15)
+key1 = os.urandom(15)
+
+PW = b64encode(key).decode('ASCII')
+PW1 = b64encode(key1).decode('ASCII')
+
+my_file = open((V)+"_"+(N)+"_Einarbeitungsheet.txt","w+")
+my_file.write((V)+" & "+(N)+"\n"+"Passwort:     "+(PW)+"\n\n")
+
+my_file.write((V)+"-"+(N)+".mellowmessage.de\n"+"Passwort:     "+(PW1)+"\n\n\n\n\n\n\n")
+
+
+my_file.write("/PFADE/ZU/SERVER/"+(V)+" oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
+my_file.write("/PFADE/ZU/SERVER/ oder Sonstige Infos\n")
